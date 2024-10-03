f52b9e311195f48b81b4186dcd5a9d0233de99db
Tom Bouquet <22462320+tombouquet@users.noreply.github.com>
2024-10-03 12:39:35 +1000

    removed old vite icon, removed navbar and also added discord server link

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import tbLogo from "./assets/TB-Logo.svg";
 import tomHeadshot from "./assets/Tom-Headshot-Square.jpg";
 import githubIcon from "./assets/github.svg";
 import discordIcon from "./assets/discord.svg";
@@ -6,16 +5,19 @@ import mailIcon from "./assets/mail.svg";
 
 const socials = [
   {
+    name: "Github",
     icon: githubIcon,
     alt: "Github icon",
     href: "https://github.com/tombouquet",
   },
   {
+    name: "Discord",
     icon: discordIcon,
     alt: "Discord icon",
-    href: "#",
+    href: "https://discord.gg/vhVTfVGXmW",
   },
   {
+    name: "Email",
     icon: mailIcon,
     alt: "Email icon",
     href: "mailto:contact@tombouquet.com",
@@ -26,20 +28,7 @@ const App = () => {
   return (
     <>
       <div className="absolute font-pulp inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] animate-pulse" />
-      <nav className="h-20 bg-stone-50 border-b-2 border-black/5">
-        <div className="container h-20 flex justify-between items-center">
-          <a href="#" target="_blank">
-            <img src={tbLogo} alt="Tom Bouquet logo" />
-          </a>
-          {/* <ul className="flex text-center">
-            <li className="w-28">About me</li>
-            <li className="w-28">Blog</li>
-            <li className="w-28">Contact Me</li>
-          </ul> */}
-        </div>
-      </nav>
-      <div className="w-full h-screen">
-        <div className="flex gap-32 justify-between items-center container py-32">
+        <div className="flex gap-32 h-screen justify-between items-center container pb-64 size-full">
           <div className="flex flex-col gap-8">
             <div>
               <h1 className="font-extrabold text-orange-600 text-5xl -ml-2">
@@ -75,7 +64,6 @@ const App = () => {
             alt="Tom Bouquet profile photo"
           />
         </div>
-      </div>
     </>
   );
 };
