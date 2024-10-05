0db762e6f17f06a0cfc57be5bf555fe1a240fc30
Tom Bouquet <22462320+tombouquet@users.noreply.github.com>
2024-10-05 18:37:40 +1000

    added image blur for slow loading profile image

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { BlurhashCanvas } from "react-blurhash";
+import { useState } from "react";
 import tomHeadshot from "./assets/Tom-Headshot-Square.jpg";
 import githubIcon from "./assets/github.svg";
 import discordIcon from "./assets/discord.svg";
@@ -25,15 +27,40 @@ const socials = [
 ];
 
 const App = () => {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  // Example Blurhash value, replace with your generated blurhash
+  const blurhash =
+    "|EK12h001N}@vz57RP%MSg00%MaLRj%Ms:V[WBs:yrxa={IpE1xubbWBjF00t7smWWWBa#ozs.kC?vn%xZR*NGxZt7RjWBIARjRjxaNGj@soW;j[%gxaM{Nb-pe.S2axWBsoRjNas:V@WURjofX8bHofRjofxuWBs:ayWU";
+
   return (
     <>
       <div className="absolute font-pulp inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] animate-pulse" />
       <div className="p-10 md:pb-64 md:container md:flex space-y-6 items-center gap-16 h-screen justify-center">
-        <img
-          src={tomHeadshot}
-          className="rounded-full size-52 md:size-64 order-last drop-shadow-xl mx-auto"
-          alt="Tom Bouquet profile photo"
-        />
+        {/* Container for the image or Blurhash */}
+        <div className="relative shrink-0 size-52 md:w-64 md:h-64 order-last drop-shadow-xl mx-auto">
+          {/* Display Blurhash while image is loading */}
+          {!imageLoaded && (
+            <BlurhashCanvas
+              hash={blurhash}
+              width={256} // Ensure it matches the size of your image container
+              height={256}
+              punch={1}
+              className="rounded-full"
+            />
+          )}
+
+          {/* Actual image */}
+          <img
+            src={tomHeadshot}
+            onLoad={() => setImageLoaded(true)}
+            className={`rounded-full w-full h-full object-cover transition-opacity duration-500 ${
+              imageLoaded ? "opacity-100" : "opacity-0"
+            }`}
+            alt="Tom Bouquet profile photo"
+          />
+        </div>
+
         <div className="space-y-6">
           <div className="text-center md:text-left">
             <h1 className="font-extrabold text-orange-600 text-5xl -ml-2">
@@ -53,6 +80,7 @@ const App = () => {
           <div className="flex gap-4 justify-center md:justify-start">
             {socials.map((social) => (
               <a
+                key={social.href}
                 href={social.href}
                 target="_blank"
                 className="size-16 bg-stone-100 group rounded-full flex hover:-translate-y-1 transition-transform items-center justify-center border-2 border-stone-300"
